a6ba4b1e057f668eb472a73199d501780ccb7d4f
LeeHyengTae <tedd911@icloud.com>
2020-05-15 08:53:22 +0900

    08:52

---
+++ src/vue/src/components/Home.vue
@@ -0,0 +1,23 @@
+<template>
+    <div id="app">
+        <layout>
+            <template #header="header">
+                <h1>{{header.title}}</h1>
+                <h3>{{header.login}}</h3>
+                <h3>{{header.signup}}</h3>
+            </template>
+        </layout>
+    </div>
+</template>
+
+<script>
+    import Layout from "@/components/common/Layout.vue"
+    export default {
+        name: "Home",
+        components : {Layout},
+        data : ()=>{
+            return {
+            }
+        }
+    }
+</script>
